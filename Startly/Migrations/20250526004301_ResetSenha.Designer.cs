bf094c9309e81e48c1cbfed71e31432ce142f828
Eduardo Professor <eggustavo+3h3@gmail.com>
2025-05-25 21:43:22 -0300

    Add ChaveResetSenha column to TB_Startup table
    Introduce a new nullable column `ChaveResetSenha` of type `Guid?` to the `TB_Startup` table for storing reset password keys. Update the model in `StartlyContextModelSnapshot.cs`, create a migration in `20250526004301_ResetSenha.cs`, and modify the model builder in `20250526004301_ResetSenha.Designer.cs` to reflect this change.

--- Startly/Migrations/20250526004301_ResetSenha.Designer.cs
+++ Startly/Migrations/20250526004301_ResetSenha.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Startly.Infra.Data.Context;
 
@@ -11,9 +12,11 @@ using Startly.Infra.Data.Context;
 namespace Startly.Migrations
 {
     [DbContext(typeof(StartlyContext))]
-    partial class StartlyContextModelSnapshot : ModelSnapshot
+    [Migration("20250526004301_ResetSenha")]
+    partial class ResetSenha
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
